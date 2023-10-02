0f60dadfe396887e29863fdfbbe26e1d45e70825
Vladimir Rochev <vladimir@rochev.me>
2023-10-02 15:44:57 +0300

    add inMemoryRepository which implements EntityRepository

--- repository.go
+++ repository.go
@@ -4,3 +4,22 @@ type EntityRepository interface {
 	Create(text string) (*Todo, error)
 	FindAll() ([]*Todo, error)
 }
+
+type inMemoryRepository struct {
+}
+
+func (r *inMemoryRepository) Create(text string) (*Todo, error) {
+	return NewTodo(text), nil
+}
+
+func (r *inMemoryRepository) FindAll() ([]*Todo, error) {
+	return []*Todo{
+		NewTodo("foo"),
+		NewTodo("bar"),
+		NewTodo("baz"),
+	}, nil
+}
+
+func NewInMemoryRepository() EntityRepository {
+	return &inMemoryRepository{}
+}
