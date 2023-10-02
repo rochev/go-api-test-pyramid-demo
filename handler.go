3364814f7f4cd8c224fc81d3a0ba4c8bccebacd6
Vladimir Rochev <vladimir@rochev.me>
2023-10-02 16:07:37 +0300

    complete handler

--- handler.go
+++ handler.go
@@ -1,11 +1,41 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
-func CreateTodoHandler(w http.ResponseWriter, _ *http.Request) {
-	_, _ = w.Write([]byte("TODO"))
+type TodoHandlers struct {
+	er EntityRepository
 }
 
-func FindAllTodosHandler(w http.ResponseWriter, _ *http.Request) {
-	_, _ = w.Write([]byte("TODO"))
+func NewHandlers(er EntityRepository) *TodoHandlers {
+	return &TodoHandlers{
+		er: er,
+	}
+}
+
+func (th *TodoHandlers) CreateTodoHandler(w http.ResponseWriter, _ *http.Request) {
+	todo, err := th.er.Create("New Todo")
+	commonHandler(w, http.StatusCreated, todo, err)
+}
+
+func (th *TodoHandlers) FindAllTodosHandler(w http.ResponseWriter, _ *http.Request) {
+	todo, err := th.er.FindAll()
+	commonHandler(w, http.StatusCreated, todo, err)
+}
+
+func commonHandler(w http.ResponseWriter, status int, data any, err error) {
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	marshal, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, _ = w.Write(marshal)
 }
